ce222d7bb2f0ed731a8d43aca9783b66b77e4f85
jmpfbmx <jmpf.bmx@gmail.com>
2025-07-22 19:51:40 +0200

    a04e: Migrate to py extract utils
    Change-Id: Ib1f0ab6d6f1029c4cf9e17502af79dc4ef9740b2
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

---
+++ extract-files.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+]
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+}
+
+blob_fixups: blob_fixups_user_type = {
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'gta7litewifi',
+    'samsung',
+    namespace_imports=namespace_imports,
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'mt6765-common', module.vendor
+    )
+    utils.run()
