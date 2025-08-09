0130f013b87107958f1ac34dda52c82073e745d6
ᑢᓰᖶᖶᖻ2029 <chikushinemathew@gmail.com>
2025-08-09 22:38:52 +0530

    a04e: bringup a04e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,19 +4,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/samsung/gta7litewifi
+DEVICE_PATH := device/samsung/a04e
 
 # Inherit from mt6765-common
 include device/samsung/mt6765-common/BoardConfigCommon.mk
 
 # Display
-TARGET_SCREEN_DENSITY := 213
+TARGET_SCREEN_DENSITY := 300
 
 # Kernel
-TARGET_KERNEL_CONFIG := gta7litewifi_defconfig
-
-# Properties
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+TARGET_KERNEL_CONFIG := rsuntk-a04e_defconfig
 
 # Inherit from the proprietary version
 include vendor/samsung/mt6765-common/BoardConfigVendor.mk
