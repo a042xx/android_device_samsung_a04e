3ad8073b99fb3e98775e385e647008811e770e8a
ＳｙｎＴａｘＳｃｕｌｐ <chikushinemathew@gmail.com>
2025-08-12 18:21:24 +0530

    a04e: switch to prebuilt

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,8 +12,5 @@ include device/samsung/mt6765-common/BoardConfigCommon.mk
 # Display
 TARGET_SCREEN_DENSITY := 300
 
-# Kernel
-TARGET_KERNEL_CONFIG := a04e_defconfig
-
 # Inherit from the proprietary version
 include vendor/samsung/mt6765-common/BoardConfigVendor.mk
